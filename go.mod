5667a9cae985ec04356a292762daa95bb81a82f1
Toan Nguyen <toannm11@viettel.com.vn>
2021-05-11 16:10:00 +0700

    add basic function tip bot

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/toannm/example-go
 
 go 1.14
 
-require github.com/lestrrat-go/jwx v1.0.4
+require (
+	github.com/lestrrat-go/jwx v1.0.4
+	github.com/turnage/graw v0.0.0-20201204201853-a177df1b5c91
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,153 @@
+// package main
+
+// import (
+// 	"github.com/toannm/example-go/json"
+// 	"github.com/toannm/example-go/jwt"
+// )
+
+// func main() {
+// 	jwt.GenJwkSignAndParse()
+// 	json.IterateObjectAndArray()
+// }
+
+//package main
+//
+//import (
+//    "crypto/tls"
+//    "fmt"
+//    "golang.org/x/net/context"
+//    cc "golang.org/x/oauth2/clientcredentials"
+//    "io/ioutil"
+//    "net/http"
+//)
+//
+//type SpecialClient struct {
+//    *http.Client
+//}
+//
+//func main() {
+//    client := NewClient(
+//        "98faa314-3a61-46e4-b011-de7c427c39cf",
+//        "SlS3PqHY7F5xzNr.wCNQW0wXkD",
+//    )
+//
+//    // the client will update its token if it's expired
+//    client.Transport =  &http.Transport{
+//        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+//    }
+//    resp, err := client.Get("https://backend.vht-iot.vn/api/groups?limit=10&offset=4")
+//
+//    if err != nil {
+//       fmt.Printf("error %v", err)
+//    }
+//
+//    body, _ := ioutil.ReadAll(resp.Body)
+//    defer resp.Body.Close()
+//
+//    // If response code is 200 it was successful
+//    if resp.StatusCode == 200 {
+//        fmt.Println("The request was successful. Response below:")
+//        fmt.Println(string(body))
+//    } else {
+//        fmt.Println("Could not perform request to the endpoint. Response below:")
+//        fmt.Println(string(body))
+//    }
+//}
+//
+//func NewClient(cid, csec string) *SpecialClient {
+//
+//    // this should match whatever service has given you
+//    // client credential access
+//    config := &cc.Config{
+//        ClientID:     cid,
+//        ClientSecret: csec,
+//        TokenURL: "http://10.55.123.115:30447/oauth2/token",
+//        Scopes:   []string{""},
+//    }
+//
+//    // you can modify the client (for example ignoring bad certs or otherwise)
+//    // by modifying the context
+//    ctx := context.Background()
+//    client := config.Client(ctx)
+//    return &SpecialClient{client}
+//}
+
+//package main
+//
+//import (
+//    "fmt"
+//    "github.com/turnage/graw/reddit"
+//)
+//
+//func main() {
+//   bot, err := reddit.NewBotFromAgentFile("reminderbot.agent", 0)
+//   if err != nil {
+//       fmt.Println("Failed to create bot handle: ", err)
+//       return
+//   }
+//
+//   harvest, err := bot.Listing("/r/TestMyBotTip", "")
+//   if err != nil {
+//       fmt.Println("Failed to fetch /r/TestMyBotTip: ", err)
+//       return
+//   }
+//
+//   for _, post := range harvest.Posts[:5] {
+//       fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
+//   }
+//}
+
 package main
 
 import (
-	"github.com/toannm/example-go/json"
-	"github.com/toannm/example-go/jwt"
+   "fmt"
+   "strings"
+   "time"
+
+   "github.com/turnage/graw"
+   "github.com/turnage/graw/reddit"
 )
 
+type reminderBot struct {
+   bot reddit.Bot
+}
+
+func (r *reminderBot) Post(p *reddit.Post) error {
+   fmt.Printf("new post: %s, self text %s", p.Title, p.SelfText)
+    fmt.Println("remind bot")
+   if strings.Contains(p.SelfText, "remind me of this post") {
+       <-time.After(10 * time.Second)
+       fmt.Printf("remind: %s", p.Author)
+       fmt.Println("send msg")
+       return r.bot.SendMessage(
+           p.Author,
+           fmt.Sprintf("Reminder: %s", p.Title),
+           "You've been reminded!",
+       )
+   }
+   return nil
+}
+
+func (r *reminderBot) Message(msg *reddit.Message) error {
+   fmt.Printf("receive message from %s content %s", msg.Author, msg.Body)
+   return nil
+}
+
 func main() {
-	jwt.GenJwkSignAndParse()
-	json.FileReadWrite()
+   if bot, err := reddit.NewBotFromAgentFile("reminderbot.agent", 10); err != nil {
+       fmt.Println("Failed to create bot handle: ", err)
+   } else {
+       fmt.Println("start bot")
+       //err = bot.SendMessage("tnm_tip_bot", "aaaaa", "asddsadas")
+       //if err != nil {
+       //    fmt.Println("failed to send message ", err)
+       //}
+       cfg := graw.Config{Subreddits: []string{"TestMyBotTip"}, Messages: true}
+       handler := &reminderBot{bot: bot}
+       if _, wait, err := graw.Run(handler, bot, cfg); err != nil {
+           fmt.Println("Failed to start graw run: ", err)
+       } else {
+           fmt.Println("graw run failed: ", wait())
+       }
+   }
 }
